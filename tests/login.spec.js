f2519aa095c77cb8c8c7ada6af830ef23f7f21db
rainstains <biladt.hakim@gmail.com>
2022-11-17 11:49:17 +0700

    update Login Function Testing

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,7 @@
 const { expect, test } = require('@playwright/test')
 var randomstring = require('randomstring')
 
-const url = "https://staging.evermosa2z.com/"
+const URL = "https://staging.evermosa2z.com/"
 
 const phoneNumber = [
     "62696969000",
@@ -17,7 +17,7 @@ const password = [
     "evermos123",
 ]
 
-async function loginValid(page) {
+async function loginValid(page,url) {
     var index = randomstring.generate({
         length: 1,
         charset: '02'
@@ -27,10 +27,8 @@ async function loginValid(page) {
     await page.getByPlaceholder('Nomor Telepon Anda').fill(phoneNumber[index]);
     await page.getByPlaceholder('Kata Sandi Anda').fill(password[index]);
     await page.getByRole('button', { name: 'Masuk' }).click();
-    await expect(page).toHaveURL(url+'catalog',5500) ;
-    await page.locator('.appNav__panel > a:nth-child(5)').click();
-    await expect(page.getByText('+'+phoneNumber[index])).toBeVisible()
-    await page.getByRole('link', { name: 'Beranda' }).click();
+    await expect(page).toHaveURL(url+'catalog',6500) ;
+    return phoneNumber[index]
 }
 
 test.describe('Login Function', () => {
@@ -40,19 +38,22 @@ test.describe('Login Function', () => {
             charset: '13'
         });
     
-        await page.goto(url+'login');
+        await page.goto(URL+'login');
         await page.getByPlaceholder('Nomor Telepon Anda').fill(phoneNumber[index]);
         await page.getByPlaceholder('Kata Sandi Anda').fill(password[index]);
         await page.getByRole('button', { name: 'Masuk' }).click();
         await expect(page.getByText('Nomor ini belum terdaftar sebagai reseller')).toBeVisible;
-        await expect(page).toHaveURL(url+'login',2500) ;
+        await expect(page).toHaveURL(URL+'login',2500) ;
     })
 
     test('Login - Positive', async ({ page }) => {
-        await loginValid(page)
+        var phone = await loginValid(page,URL)
+        await page.locator('.appNav__panel > a:nth-child(5)').click();
+        await expect(page.getByText('+'+phone)).toBeVisible()
+        await page.getByRole('link', { name: 'Beranda' }).click();
     })
     
 })
 
-
+module.exports = { loginValid, URL }
 
